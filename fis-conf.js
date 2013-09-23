8933386863db5d6d23a282e69e1b26a482ff157f
zhangyunlong <zhangyunlong@baidu.com>
2013-09-23 11:36:11 +0800

    aaa

--- fis-conf.js
+++ fis-conf.js
@@ -6,3 +6,14 @@ fis.config.merge({
 
 //上面的配置等价于：
 //fis.config.set('project.include', 'src/**');
+
+fis.config.set('roadmap.path', [
+    {
+        //fis会自动产出一个资源表
+        reg : 'map.json',
+        //这里不要它输出
+        release : false
+        //也可以调整输出到别的地方，比如/src/map.json
+        //release : 'src/$&'
+    }
+]);
